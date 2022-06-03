8ce54bf71c6c93de4cf159f81767e2b678c00036
Richizero <richizero@hotmail.com>
2022-06-03 10:33:40 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <ScrollView>
+      <Text style={{ fontSize: 50 }}>hola</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>mundo</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>como</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>estas</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>yo muy</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>bien</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>y tu</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>tu muy bien</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>bueno</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>adios</Text>
+      <Image source={logo} />
+
+    </ScrollView>
+   
+    
   );
 }
 
